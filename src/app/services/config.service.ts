c6ed5624718f0614b6b7089e25d8f1d2c5c22652
santhan <santhan.v@tickingminds.com>
2026-02-16 10:58:38 +0530

    changed render link

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -6,7 +6,8 @@ import { Injectable } from '@angular/core';
 export class ConfigService {
 
   private readonly localUrl = 'http://127.0.0.1:8001';
-  private readonly productionUrl = 'https://backend-python-production-0cc8.up.railway.app';
+  // private readonly productionUrl = 'https://backend-python-production-0cc8.up.railway.app';
+  private readonly productionUrl = 'https://backend-hgdv.onrender.com';
 
   getApiUrl(path: string): string {
 
